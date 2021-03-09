a13d13f42a1875fb1c9a506c55c3648f95c905a6
mohammadreza booshehri <mrbooshehri@gmail.com>
2021-03-09 22:38:18 +0300

    synonym finder modified

--- dmenu/dmenu-dict.sh
+++ dmenu/dmenu-dict.sh
@@ -1,20 +1,9 @@
 #! /bin/bash
 
-mkdir -p $HOME/.cache/jq_dic
-
 if [ -z $1 ]; then
   word=$(echo "" | dmenu -p "Look up for: ")
 else
   word=$1
 fi
-
-baseurl="https://api.dictionaryapi.dev/api/v2/entries/en_US/"
-cachedir="$HOME/.cache/jq_dic"
-
-#curl -s $baseurl$word > $cachedir/temp.bw
-
-#jq -r '.[].meanings[] |.partOfSpeech, .definitions[].definition, .definitions[].synonyms[]?' $cachedir/temp.bw > $cachedir/filterd.bw
-
-## Just synonyms
 
 printf "$(curl -s $baseurl$word | jq -r '.[].meanings[] | .definitions[].synonyms[]?')" | dmenu -l 15 -p "Synonyms of [ $word ]"
