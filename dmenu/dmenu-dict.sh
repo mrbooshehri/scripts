46129fe563a2f25d8742c3d34bfdf0f7110934ad
mohammadreza booshehri <mrbooshehri@gmail.com>
2021-03-10 14:09:37 +0300

    synonym finder bugs fixed

--- dmenu/dmenu-dict.sh
+++ dmenu/dmenu-dict.sh
@@ -1,15 +1,13 @@
 #! /bin/bash
 
 baseurl="https://api.dictionaryapi.dev/api/v2/entries/en_US/"
+word=$(dmenu -p "Look up for:"<&-)
 
-if [ -z $1 ]; then
-  word=$(echo "" | dmenu -p "Look up for: ")
-else
-  word=$1
+if [ ! -z $word ]; then
+	selected=$(echo -n "$(curl -s $baseurl$word | jq -r '.[].meanings[] | .definitions[].synonyms[]?')" \
+	  | dmenu -l 15 -p "Synonyms of [ $word ]")  
+	if [ ! -z $selected ]; then
+		echo $selected | xsel -bi
+		notify-send -t 2000 -u normal "[ $selected ] copied to clipboard."
+	fi
 fi
-
-selected=$(echo -n "$(curl -s $baseurl$word | jq -r '.[].meanings[] | .definitions[].synonyms[]?')" | dmenu -l 15 -p "Synonyms of [ $word ]")  
-
-echo $selected | xsel -bi
-
-notify-send -t 2000 -u normal "$selected copied to clipboard."
