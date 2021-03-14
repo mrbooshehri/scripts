f92986d11e7a72112e6c74b5a48112273da640f1
mohammadreza booshehri <mrbooshehri@gmail.com>
2021-03-14 13:43:55 +0300

    dmneu process killer script added

---
+++ dmenu/dmenu-kill-ps.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+# 1) ps get processes belongs to mhmd user
+# 2) sed remove the first line of the output (heading)
+# 3) awk print (PID, CPU%, MEM%, CMD) with " | " delimiter
+# 4) dmenu show the results as a menu
+# 5) cut get the first element of picked line (PID)
+PID=$(ps -U mhmd u | sed '1d' | awk '{print $2, " | ", $3, " | ", $4, " | ", $11}' | dmenu -l 15 | cut -d " " -f 1)
+
+# check if $PID is not null
+if [ ! -z $PID ]; then
+  # kill the selected process with default signal (-9)
+	kill $PID
+	# check the result of kill command and send related notification
+	case $? in
+	  "0")
+	    notify-send -t 2000 -u low "Process terminated successfully";;
+	  "1")
+	    notify-send -t 2000 -u urgent "Process termination failed";;
+	  "64")
+	    notify-send -t 2000 -u normal "Process terminated partial successful";;
+	esac
+fi
