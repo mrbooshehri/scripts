3a2d72bc6edebcda96019a35cde10bb526649b75
mohammadreza booshehri <mrbooshehri@gmail.com>
2021-03-22 17:19:35 +0300

    force curls to use IPv4

--- dmenu/dmenu-dict.sh
+++ dmenu/dmenu-dict.sh
@@ -4,7 +4,7 @@ baseurl="https://api.dictionaryapi.dev/api/v2/entries/en_US/"
 word=$(dmenu -p "Look up for:"<&-)
 
 if [ ! -z $word ]; then
-	selected=$(echo -n "$(curl -s $baseurl$word | jq -r '.[].meanings[] | .definitions[].synonyms[]?')" \
+	selected=$(echo -n "$(curl -4s $baseurl$word | jq -r '.[].meanings[] | .definitions[].synonyms[]?')" \
 	  | dmenu -l 15 -p "Synonyms of [ $word ]")  
 	if [ ! -z $selected ]; then
 	  	echo "$word = $selected" >> ~/notes/vocab.md
