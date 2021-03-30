48e99585791c8e80d20c097a34408393e99d42f6
mohammadreza booshehri <mrbooshehri@gmail.com>
2021-03-30 19:13:52 +0300

    volume script adpated with all stats

--- dwmblocks/volume.sh
+++ dwmblocks/volume.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-echo  $(amixer -c 1 -D pulse sget Master |grep 'Left:')| awk -F '[][]' '{print $2}'
-#VOL=$(amixer -c 1 -D pulse sget Master |grep 'Left:')| awk -F '[][]' '{print $2}'
-#STAT=$(amixer -c 1 -D pulse sget Master |grep 'Left:')| awk -F '[][]' '{print $4}'
-#case $STAT in 
-#  off)
-#	echo "婢 ${VOL}"
-#    ;;
-#  on)
-#	echo "墳 ${VOL}" 
-#    ;;
-#esac
+VOL=$(amixer -c 1 -D pulse sget Master | grep 'Left:' | awk -F '[][]' '{print $2}')
+STAT=$(amixer -c 1 -D pulse sget Master | grep 'Left:' | awk -F '[][]' '{print $4}')
+case $STAT in 
+  off)
+	echo " ${VOL}"
+    ;;
+  on)
+    if [ $VOL == "0%" ]
+    then
+	echo " ${VOL}" 
+    else
+	echo " ${VOL}" 
+    fi
+    ;;
+esac
