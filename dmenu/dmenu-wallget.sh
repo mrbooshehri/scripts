91667d42a4a9d685b8ab250db9d5dda904f658be
mohammadreza booshehri <mrbooshehri@gmail.com>
2021-04-15 21:12:04 +0300

    apikey/tagsearch added

--- dmenu/dmenu-wallget.sh
+++ dmenu/dmenu-wallget.sh
@@ -6,44 +6,62 @@
 #|_| |_| |_|_|  |_.__/ \___/ \___/|___/_| |_|\___|_| |_|_|  |_|
 
 #! /bin/bash
+
+# Check if necessary programs are installed
+for prog in dmenu jq sxiv notify-send; do
+	[ ! "$(which "$prog")" ] && echo "Please install $prog!" && exit 1
+done
+
+# Default directories
+dir_conf="${XDG_CONFIG_HOME:-$HOME/.config}/wallget"
 dir_repo="/mnt/1TB/Pictures/Wallpaper.repo"
 dir_tmp="/tmp/wallpaper"
-maxpage=5
 
-mkdir -p $dir_tmp
+# API Key
+api=9VxdYyf7irmZl8tIv6x6BSy7bylSKtUb
 
-tagoptions="#4K\n#artwork\n#nature\n#fantasy art\n#technology\n#minimalism\nabstract"
-if [ -z $1 ]; then
-  query=$(echo -e $tagoptions | dmenu -p "Search wallhaven: ")
+# Create .config/wallget if it does not exist to prevent
+# the program from not functioning properly
+[ ! -d "$dir_conf" ] && echo "Creating $dir_conf" && mkdir -p "$dir_conf"
+
+## Sample tag
+header="#Category\t\t\tTag"
+default="minimalism\t\t\t2278"
+
+# If subbreddit.txt does not exist, create it to prevent
+# the program from not functioning properly
+[ ! -f "$dir_conf/config.txt" ] && echo -e "$header\n$default" >> "$dir_conf/config.txt"
+
+# If no argument is passed
+if [ -z "$1" ]; then
+	# Ask the user to enter a subreddit
+	query=$(tail -n +2 $dir_conf/config.txt | awk '{print $1}' | dmenu )
+	# Fetch tag number from config file if it exists
+	[ ! -z "$(grep $query $dir_conf/config.txt)" ] && $query=$(awk '/$query/ {print $2} $dir_conf/config.txt')
+	# If nothing was chosen, exit
+	[ -z "$query" ] && exit 1
 else
-  query=$1
+	query="$1"
 fi
+echo $query
 
-#if [ -z $query ]; then
-#  exit
-#fi
-[ -z $query ] && exit 1
-
+# Asking for sorting option
 sortoptions="date_added\nrelevance\nrandom\nfavorites\ntoplist"
 sorting=$(echo -e $sortoptions | dmenu -p "Sort order: ")
 
-query="$(sed 's/#//g' <<<$query)"
-query="$(sed 's/ /+/g' <<<$query)"
+notify-send "Wallget" "  Downloading..."
 
-notify-send "  Downloading..."
 for i in $(seq 1 2);
 do
-  curl -s https://wallhaven.cc/api/v1/search\?atleast\=1920x1080\&sorting\=$sorting\&q\=$query\&page\=$i > tmp.txt
-  #page=$(cat tmp.txt | jq '.' | grep -Eoh "https:\/\/w\.wallhaven.cc\/full\/.*(jpg|png)\b")
+  curl -s https://wallhaven.cc/api/v1/search\?apikey\=$api\&atleast\=1920x1080\&sorting\=$sorting\&q\=$query\&page\=$i > tmp.txt
   page=$(cat tmp.txt | jq --raw-output '.data[].path')
-  wget -qnc -P $dir_tmp $page
+  wget -nc -P $dir_tmp $page
 done
 
 rm tmp.txt
-notify-send "  All files downloaded"
+notify-send "Wallget" "  All files have downloaded"
 
 sxiv -t $dir_tmp/*
 
 mv $dir_tmp/* $dir_repo
-notify-send "  Files moved"
-
+notify-send "Wallget" "  Files moved to $dir_repo"
