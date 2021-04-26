2faa3d0af299d6e60bea7f64680d8ddd3fadd399
mohammadreza booshehri <mrbooshehri@gmail.com>
2021-04-26 15:20:21 +0300

    add query as header

--- dmenu/dmenu-lyrics.sh
+++ dmenu/dmenu-lyrics.sh
@@ -5,8 +5,9 @@ file_tmp_txt=/tmp/$basename.txt
 file_tmp_pdf=/tmp/$basename.pdf
 
 query=$(dmenu -p "Lyrics search: " <&-)
+echo -e "# $query\n\n" > $file_tmp_txt
 
-~/scripts/general/google-lyric.sh $query > $file_tmp_txt
+~/scripts/general/google-lyric.sh $query >> $file_tmp_txt
 sed -i 's/$/  /' $file_tmp_txt
 pandoc -V geometry:margin=1in $file_tmp_txt -o $file_tmp_pdf
 zathura $file_tmp_pdf
