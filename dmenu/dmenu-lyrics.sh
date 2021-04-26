21169887233129c5816487e522458923c70bdfd9
mohammadreza booshehri <mrbooshehri@gmail.com>
2021-04-26 10:57:44 +0300

    lyrics grabber added

---
+++ dmenu/dmenu-lyrics.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+basename=lyrics
+file_tmp_txt=/tmp/$basename.txt
+file_tmp_pdf=/tmp/$basename.pdf
+
+query=$(dmenu -p "Lyrics search: " <&-)
+
+~/scripts/general/google-lyric.sh $query > $file_tmp_txt
+sed -i 's/$/  /' $file_tmp_txt
+pandoc -V geometry:margin=1in $file_tmp_txt -o $file_tmp_pdf
+zathura $file_tmp_pdf
