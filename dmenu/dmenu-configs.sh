cee22dea53865f2bd42ae9f4dab0f4eb80482a16
mohammadreza booshehri <mrbooshehri@gmail.com>
2021-05-03 22:21:48 +0300

    follow symlinks

--- dmenu/dmenu-configs.sh
+++ dmenu/dmenu-configs.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-FILE=$(find ~/.config/* -mindepth 1 -maxdepth 2 | dmenu -l 15)
+FILE=$(find -L ~/.config/* -mindepth 1 -maxdepth 2 | dmenu -l 15)
 if [ -n "$FILE" ];then
 	alacritty -e vim $FILE
 fi
