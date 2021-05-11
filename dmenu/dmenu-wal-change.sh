4d25e971d96c82ac3b035f112a6a512f3c06084a
mohammadreza booshehri <mrbooshehri@gmail.com>
2021-05-11 11:17:20 +0300

    bug fixed

--- dmenu/dmenu-wal-change.sh
+++ dmenu/dmenu-wal-change.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 dir_repo=/mnt/1TB/Pictures/Wallpaper.repo
-feh --bg-fill "$(find $dir_repo -regextype egrep -regex '*.jpg' | shuf | head -1)"
+feh --bg-fill "$(find $dir_repo -regextype egrep -regex '.*.(jpg|png)' | shuf | head -1)"
