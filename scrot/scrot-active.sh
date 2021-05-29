a220c2990bc3050c244a3fdfb43c0d3a1c2436a5
mohammadreza booshehri <mrbooshehri@gmail.com>
2021-05-29 13:52:38 +0300

    rewrite the screen, make messages meaningful

--- scrot/scrot-active.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
-scrot ~/Pictures/%Y-%m-%d_screenshot.png -u -q 100 -e 'notify-send -t 3000 "Screenshot taken" "$f"'
--- scrot/scrot.sh
+++ scrot/scrot.sh
@@ -1,2 +1,22 @@
-#! /bin/bash
-scrot ~/Pictures/%Y-%m-%d_screenshot.png -q 100 -e 'notify-send -t 3000 "Screenshot taken" "$f"'
+#! /usr/bin/env bash
+
+# swich		action
+# 		entire screen
+# -a		active window
+# -s		selection mode
+
+dir_repo=~/Pictures/screenshots
+
+case $1 in 
+  "-a") scrot $dir_repo/%Y-%m-%d_screenshot.png -u -q 100 \
+  	-e 'xclip -selection clipboard -t image/png "$f"; \
+   	notify-send -t 3000 "Scrot [ Active window ]" "Screenshot taken\n$n"' ;;
+
+  "-s") sleep 0.1 && scrot $dir_repo/%Y-%m-%d_screenshot.png -f -l width=3 -s -q 100 \
+      	-e 'xclip -selection clipboard -t image/png "$f"; \
+      	notify-send -t 3000 "Scrot [ Selected area]" "Screenshot taken\n$n"';;
+
+  *) scrot $dir_repo/%Y-%m-%d_screenshot.png -q 100 \
+     -e 'notify-send -t 3000 "Scrot [ Entire screen ]" "Screenshot taken\n$n"' ;;
+
+esac
