fd4c8cf7952e1379e5ed07ed304566c8879c2897
mohammadreza booshehri <mrbooshehri@gmail.com>
2021-09-26 13:20:25 +0300

    imporve url regex

--- dmenu/dmenu-firefox-helper.sh
+++ dmenu/dmenu-firefox-helper.sh
@@ -3,7 +3,7 @@
 dir_conf=~/.config/firefox-helper
 file_bookmarks=$dir_conf/firefox-bookmarks
 
-# Chechk conf folder and bookmark
+# Check conf folder and bookmark
 mkdir -p $dir_conf
 test -f $file_bookmarks || touch file_bookmarks
 
@@ -14,9 +14,10 @@ printf "%s\n" "$(sort $file_bookmarks)" > $file_bookmarks
 input=$(dmenu -p "Go: " -l 5 < $file_bookmarks)
 # If process is canceled 
 [ -z "$input" ] && exit 1
-# Add url if it does not exist in bookmark file
-if [[ $input =~ ".com" ]]
+# Check input if it's url or not
+if [[ $input =~ "." ]]
 then
+# Add url if it does not exist in bookmark file
   grep "$input" $file_bookmarks > /dev/null ||  echo "$input" >> "$file_bookmarks"
   firefox "$input"
 else
