2177b8bd3799dce48d6ebf3705d11e3ac78c8f2c
mohammadreza booshehri <mrbooshehri@gmail.com>
2021-09-30 18:38:21 +0300

    base arch install script

---
+++ test/base-efi.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+ln -sf /usr/share/zoneinfo/Europe/Istanbul /etc/localtime
+hwclock --systohc
+sed -i '177s/.//' /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "arch" >> /etc/hostname
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1       localhost" >> /etc/hosts
+echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
+echo root:password | chpasswd
+
+# You can remove the tlp package if you are installing on a desktop or vm
+
+pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync acpi acpi_call tlp bridge-utils dnsmasq sof-firmware acpid os-prober ntfs-3g 
+
+pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
+
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+
+systemctl enable NetworkManager
+systemctl enable bluetooth
+systemctl enable cups.service
+systemctl enable sshd
+systemctl enable avahi-daemon
+systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
+systemctl enable reflector.timer
+systemctl enable fstrim.timer
+systemctl enable acpid
+
+useradd -m mhmd
+echo ermanno:password | chpasswd
+
+echo "mhmd ALL=(ALL) ALL" >> /etc/sudoers.d/mhmd
+
+
+printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
+
