e8f659b1d72e23626476c1dbae33a4880090e1ab
mohammadreza booshehri <mrbooshehri@gmail.com>
2021-10-03 13:47:51 +0300

    integration and improvment

--- arch/dwm.sh
+++ arch/dwm.sh
@@ -4,32 +4,42 @@
 aur_helper=true
 install_ly=false
 install_lightdm=true
-gen_xprofile=false
 
+# Insall AUR helper
 if [[ $aur_helper = true ]]; then
-  cd /tmp
+  printf 'Installing yay...\n'
+  cd /tmp || echo "Faild to change dir to /tmp"
   git clone https://aur.archlinux.org/yay-git.git
   cd yay-git/;makepkg -si --noconfirm;cd
+  printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' =
 fi
 
 # Install packages
+printf 'Installing packages...\n'
 sudo pacman -S xorg-xinit xorg-server xorg-xprop xorg-xsetroot xorg-xrandr xorg-xrdb xorg-xclipboard xorg-setxkbmap xorg-docs libxinerama libx11 webkit2gtk yajl mpd kitty 
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' =
 
 if [[ $aur_helper = true ]]; then
+  printf 'Installing packages from AUR...\n'
   yay -Syyu 
   yay -S libxft-bgra lightdm-webkit-theme-aether 
+  printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' =
 fi
 
 # Pull Git repositories and install
+printf 'Installing suckless tools...\n'
 cd /tmp
 repos=( "dmenu" "dwm" "dwmblocks" )
 for repo in ${repos[@]}
 do
   git clone "https://github.com/mrbooshehri/$repo.git"
   cd $repo;make;sudo make install;cd ..
+  printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 done
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' =
 
 # XSessions and dwm.desktop
+printf 'Creating dwm.desktop...\n'
 if [[ ! -d /usr/share/xsessions ]]; then
   sudo mkdir /usr/share/xsessions
 fi
@@ -44,18 +54,23 @@ Icon=dwm
 Type=XSession
 EOF
 sudo cp ./temp /usr/share/xsessions/dwm.desktop;rm ./temp
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' =
 
 # Install lightdm
 if [[ $install_lightdm = true ]]; then
+  printf 'Installing lightdm...\n'
   sudo pacman -S lightdm lightdm-webkit2-greeter
   sudo systemctl enable lightdm 
+  printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' =
 fi
 
 # Install ly
 if [[ $install_ly = true ]]; then
+  printf 'Installing ly display manager...\n'
   git clone https://aur.archlinux.org/ly
   cd ly;makepkg -si
   sudo systemctl enable ly
+  printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' =
 fi
 
 printf "\e[1;32mDone! you can now reboot.\e[0m\n"
--- arch/meload.sh
+++ arch/meload.sh
@@ -5,24 +5,21 @@ dir_home=$HOME
 dir_src=$dir_home/.src
 dir_dots=$dir_home/.dotfiles
 
-## update system
-sudo pacman -Syyu --noconfirm
-
 ## install base packages
-sudo pacman -S --noconfirm alacritty picom conky kdeconnect sxiv zathura mpv moc redshift sxhkd thunar virtualbox obs-studio code telegram-desktop torbrowser-launcher scrot lxappearance speedcrunch gedit firefox emacs bitwarden gimp youtube-dl  zsh jq syncthing stow transmission-cli dunst polkit-gnome 
+sudo pacman -S --noconfirm alacritty picom conky kdeconnect sxiv zathura mpv moc redshift sxhkd thunar virtualbox obs-studio code telegram-desktop torbrowser-launcher scrot lxappearance speedcrunch gedit firefox emacs bitwarden gimp youtube-dl zsh jq syncthing stow transmission-cli dunst polkit-gnome 
 
 ## update AUR packages
 yay -Syu
 
 ## install from AUR
-yay -S anydesk-bin safeeyes balena-etcher stremio optimus-manager-qt xmenu betterlockscreen python-pywal otpclient pup optimus-manager optimus-manager-qt
+yay -S anydesk-bin safeeyes balena-etcher stremio xmenu betterlockscreen python-pywal otpclient pup #optimus-manager optimus-manager-qt
 
 ## persinal scripts
 git clone https://github.com/mrbooshehri/scripts.git
 git clone https://github.com/mrbooshehri/.dotfiles.git
 
 ## reload config files with stow
-cd "$dir_dots" || return
+cd "$dir_dots" || echo "Falied to change dirctory to $dir_dots"
 stow --adopt -vt ~ * 
 
 ## xinit
@@ -33,8 +30,9 @@ stow --adopt -vt ~ *
 
 ## useful tools
 mkdir -p "$dir_src"
-git clone https://github.com/pystardust/ytfzf.git "$dir_src"
-git clone https://github.com/Bugswriter/pirokit.git "$dir_src"
-git clone https://github.com/egeesin/alacritty-color-export.git "$dir_src"
-git clone https://github.com/GideonWolfe/Zathura-Pywal.git "$dir_src"
-git clone https://github.com/agnipau/telegram-palette-gen.git "$dir_src"
+cd "$dir_src" || echo "Falied to change dirctory to $dir_src"
+git clone https://github.com/pystardust/ytfzf.git
+git clone https://github.com/Bugswriter/pirokit.git
+git clone https://github.com/egeesin/alacritty-color-export.git
+git clone https://github.com/GideonWolfe/Zathura-Pywal.git
+git clone https://github.com/agnipau/telegram-palette-gen.git
