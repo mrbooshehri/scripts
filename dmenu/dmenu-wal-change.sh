9d437c641e21d4cea81708ad5afdcf7af6d9e477
mohammadreza booshehri <mrbooshehri@gmail.com>
2021-10-06 10:32:59 +0300

    betterlockscreen update walpaper

--- dmenu/dmenu-wal-change.sh
+++ dmenu/dmenu-wal-change.sh
@@ -1,3 +1,5 @@
 #! /usr/bin/env bash
 dir_repo=/mnt/1TB/Pictures/Wallpaper.repo
-feh --bg-fill "$(find $dir_repo -regextype egrep -regex '.*.(jpg|png)' | shuf | head -1)"
+image=$(find $dir_repo -regextype egrep -regex '.*.(jpg|png)' | shuf | head -1)
+feh --bg-fill $image
+betterlockscreen -u "$image" 
