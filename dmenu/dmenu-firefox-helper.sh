239524382613057e59716d9b3255bdc395fd6e73
mohammadreza booshehri <mrbooshehri@gmail.com>
2021-10-12 12:07:13 +0300

    minor improvement

--- dmenu/dmenu-firefox-helper.sh
+++ dmenu/dmenu-firefox-helper.sh
@@ -2,6 +2,7 @@
 # Varaibles
 dir_conf=~/.config/firefox-helper
 file_bookmarks=$dir_conf/firefox-bookmarks
+regex='((https?|ftp|file)://)?(www.)?[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]'
 
 # Check conf folder and bookmark
 mkdir -p $dir_conf
@@ -15,13 +16,15 @@ input=$(dmenu -p "Go: " -l 5 < $file_bookmarks)
 # If process is canceled 
 [ -z "$input" ] && exit 1
 # Check input if it's url or not
+
 if [[ $input =~ "." ]]
 then
-# Add url if it does not exist in bookmark file
-  grep "$input" $file_bookmarks > /dev/null ||  echo "$input" >> "$file_bookmarks"
-  firefox "$input"
+  if [[ $(echo $input | wc -w) == 1 ]]
+  then
+    # Add url if it does not exist in bookmark file
+      grep "$input" $file_bookmarks > /dev/null ||  echo "$input" >> "$file_bookmarks"
+      firefox "$input"
+  fi
 else
   firefox "https://searx.bar/search?q=$input"
 fi
-
-
