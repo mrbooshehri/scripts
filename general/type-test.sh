c4fae2124a4732e8095080f850fff3dd0beaf387
mohammadreza booshehri <mrbooshehri@gmail.com>
2021-10-14 11:38:56 +0300

    adjust with kitty

--- general/type-test.sh
+++ general/type-test.sh
@@ -6,4 +6,5 @@ url=https://www.brainyquote.com/topics/random-quotes
 
 curl -s4 "$url" | pup '#quotesList a' | pup '.oncl_q' | sed -E "/<.*>/d;s/&#39;/\'/g" | shuf | head -5 > "$file_tmp"
 
-alacritty -e mitype -f "$file_tmp"
+# for kitty you have to copy 'mitype' binary to /usr/bin/
+kitty mitype -f $file_tmp
