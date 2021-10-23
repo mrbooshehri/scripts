329e54becc65f04fd6ac9d6cdd1bb81d77c37e49
mohammadreza booshehri <mrbooshehri@gmail.com>
2021-10-23 17:20:25 +0300

    v1

---
+++ dmenu/dmenu-directory-helper.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+choice=$(echo -e "/home\n/mnt" | dmenu -p "Select path: ")
+
+[ -z "$choice" ] && exit 1
+
+case $choice in
+  "/home") select=$(find ~ -maxdepth 3 -type d 2>/dev/null | cut -d "/" -f 4- |  dmenu -l 15)
+    dir_home=/home/mhmd
+    ;;
+  "/mnt") select=$(find /mnt/1TB/ -maxdepth 3 -type d 2>/dev/null | cut -d "/" -f 4- | dmenu -l 15) 
+    dir_home=/mnt/1TB
+    ;;
+esac
+
+[ -z "$select" ] && exit 1
+
+thunar "$dir_home/$select"
