ea719d25e14e07e03c43adc0ee587a135d7175a2
mohammadreza booshehri <mrbooshehri@gmail.com>
2021-10-23 17:27:46 +0300

    remove first blank item, sorted output

--- dmenu/dmenu-directory-helper.sh
+++ dmenu/dmenu-directory-helper.sh
@@ -4,10 +4,10 @@ choice=$(echo -e "/home\n/mnt" | dmenu -p "Select path: ")
 [ -z "$choice" ] && exit 1
 
 case $choice in
-  "/home") select=$(find ~ -maxdepth 3 -type d 2>/dev/null | cut -d "/" -f 4- |  dmenu -l 15)
+  "/home") select=$(find ~ -maxdepth 3 -type d 2>/dev/null | cut -d "/" -f 4- | tail -n +2 | sort | dmenu -l 15)
     dir_home=/home/mhmd
     ;;
-  "/mnt") select=$(find /mnt/1TB/ -maxdepth 3 -type d 2>/dev/null | cut -d "/" -f 4- | dmenu -l 15) 
+  "/mnt") select=$(find /mnt/1TB/ -maxdepth 3 -type d 2>/dev/null | cut -d "/" -f 4- | tail -n +2 | sort | dmenu -l 15) 
     dir_home=/mnt/1TB
     ;;
 esac
