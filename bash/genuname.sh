4e368b94ef56b8b5b7b1bb846b77bcddb02fa8b6
mohammadreza booshehri <mrbooshehri@gmail.com>
2021-10-25 22:29:01 +0300

    bug fixed

--- bash/genuname.sh
+++ bash/genuname.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-username=$(shuf ./usernames.txt | head -1)
+
+dir_conf=$HOME/.config/genuname 
+mkdir -p $dir_conf 
+
+if [ ! -f "$dir_conf/usernames.txt" ]; then
+  echo "Getting usernames list:"
+  sleep 1
+  wget -O "$dir_conf/usernames.txt" "https://raw.githubusercontent.com/mrbooshehri/scripts/master/bash/usernames.txt"
+  printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' =
+fi
+
+username=$(shuf "$dir_conf/usernames.txt" | head -1)
 number=$((1 + $RANDOM % 1000)) 
 
 case $1 in
