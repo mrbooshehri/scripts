7689c4afe24ba5aa5e0797127c316b12667e410c
mohammadreza booshehri <mrbooshehri@gmail.com>
2021-11-16 14:15:36 +0300

    bug fixed

--- dmenu/dmenu-directory-helper.sh
+++ dmenu/dmenu-directory-helper.sh
@@ -7,17 +7,19 @@ case $choice in
   "/home") 
     dir_home=$HOME
     select=$(find $dir_home -maxdepth 3 -type d 2>/dev/null | cut -d "/" -f 4- | tail -n +2 | sort | dmenu -l 15)
+    [ -z "$select" ] && exit 1
+    thunar "$dir_home/$select"
     ;;
   "/mnt") 
     dir_home=/mnt/1TB
     select=$(find $dir_home -maxdepth 3 -type d 2>/dev/null | cut -d "/" -f 4- | tail -n +2 | sort | dmenu -l 15) 
+    [ -z "$select" ] && exit 1
+    thunar "$dir_home/$select"
     ;;
   *)
     [[ -d $choice ]] && dir_home="$choice" || notify-send "Directory Helper" "Invalid path"
     select=$(find $dir_home -maxdepth 3 -type d 2>/dev/null | tail -n +2 | sort | dmenu -l 15) 
+    [ -z "$select" ] && exit 1
+    thunar "$select"
     ;;
 esac
-
-[ -z "$select" ] && exit 1
-
-thunar "$dir_home/$select"
