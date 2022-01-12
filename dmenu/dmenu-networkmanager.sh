fc2b8f9feaee0777bdc8307f94df364652236014
mohammadreza booshehri <mrbooshehri@gmail.com>
2022-01-12 14:08:49 +0300

    error handeling added

--- dmenu/dmenu-networkmanager.sh
+++ dmenu/dmenu-networkmanager.sh
@@ -4,12 +4,22 @@
 CHOICE=$(nmcli dev wifi list | tail -n +2 | cut -d '*' -f2 |  awk '{print " [ ",$2," ] ", " [ ",$8," ] "}' | dmenu -l 15 -p "Select your network: ")
 [ -z "$CHOICE" ] && exit 0
 IFACE=$(echo $CHOICE | awk '{print $2}')
-# Get password
-PASSWORD=$(dmenu -p "Enter [ $IFACE ] password : " <&-)
-[ -z "$PASSWORD" ] && exit 0
-
-# Connect to the network
-RESPONS=$(nmcli dev wifi connect "$IFACE" password "$PASSWORD")
-
-echo $RESPONS
-
+if [ "$(nmcli con show | grep -c $IFACE)" == 1 ];then
+  MESSAGE=$(nmcli dev wifi connect $IFACE)
+  if [ "$(echo $MESSAGE | awk -F ':' '{print $1}')" == "Error" ];then
+    notify-send "nmcli" "$(echo $MESSAGE | awk -F ': ' '{print $2}').$(echo $MESSAGE | awk -F ')' '{print $2}')"
+  else
+    notify-send "nmcli" "Successfully conneceted to [ $IFACE ]"
+  fi
+else
+  # Get password
+  PASSWORD=$(dmenu -p "Enter [ $IFACE ] password : " <&-)
+  [ -z "$PASSWORD" ] && exit 0
+  # Connect to the network
+  MESSAGE=$(nmcli dev wifi connect "$IFACE" password "$PASSWORD")
+  if [ "$(echo $MESSAGE | awk -F ':' '{print $1}')" == "Error" ];then
+    notify-send "nmcli" "$(echo $MESSAGE | awk -F ': ' '{print $2}').$(echo $MESSAGE | awk -F ')' '{print $2}')"
+  else
+    notify-send "nmcli" "Connected successfully"
+  fi
+fi
